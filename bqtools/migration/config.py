6c428002c4c11b8f532c23e3c47dc2bf7c09c5a5
Vahid Hallaji <vhallaji@buenosystems.com.au>
2020-07-06 13:43:30 +1000

    fix: fallback to default google cloud location

--- bqtools/migration/config.py
+++ bqtools/migration/config.py
@@ -2,8 +2,8 @@ from os import path, environ, getcwd
 
 
 DIRECTORY = path.join(getcwd(), 'migrations')
-DATASET = '{}.{}'.format(environ['GOOGLE_CLOUD_PROJECT'], 'bqhistory')
+DATASET = '{}.{}'.format(environ['GOOGLE_CLOUD_PROJECT'], 'bqtools')
 TABLE = '{}.{}'.format(DATASET, 'schema_history')
-LOCATION = environ['GOOGLE_CLOUD_LOCATION'] if environ['GOOGLE_CLOUD_LOCATION'] is not None else 'australia-southeast1'
+LOCATION = environ.get('GOOGLE_CLOUD_LOCATION', 'australia-southeast1')
 SEPARATOR = '__'
 EXTENSION = '.sql'
